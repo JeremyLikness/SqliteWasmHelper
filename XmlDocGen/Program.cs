86cde68efc4288e5019d581f40bda2b92534f864
Jeremy Likness <jeremy@jeremylikness.com>
2022-04-19 07:32:31 -0700

    Updates to documentation generation #skip_ci

--- XmlDocGen/Program.cs
+++ XmlDocGen/Program.cs
@@ -1,3 +1,43 @@
 ﻿using XmlDocMarkdown.Core;
 
-return XmlDocMarkdownApp.Run(args);
+var generate = XmlDocMarkdownApp.Run(args);
+
+var target = Path.Combine(
+    Directory.GetCurrentDirectory(),
+        @"SqliteWasmHelper\docs\");
+
+Console.WriteLine($"Version stamping at path {target}");
+
+if (Directory.Exists(target))
+{
+    var version = $"Version {ThisAssembly.AssemblyInformationalVersion} generated on {DateTime.Now}.";
+    Console.WriteLine($"Stamping with: {version}");
+    var queue = new Stack<string>();
+    queue.Push(target);
+    while (queue.Count > 0)
+    {
+        var dir = queue.Pop();
+        foreach (var subdir in Directory.EnumerateDirectories(dir))
+        {
+            queue.Push(subdir);
+        }
+
+        foreach (var file in Directory.EnumerateFiles(dir)
+            .Where(f => Path.GetExtension(f).ToLower() == ".md"))
+        {
+            var text = File.ReadAllLines(file);
+            var newText = text.Union(new[]
+            {
+                $"{Environment.NewLine}",
+                version
+            });
+            File.WriteAllLines(file, newText);
+        }
+    }
+}
+else
+{
+    throw new DirectoryNotFoundException(target);
+}
+
+return generate;
